a8ebe73a6b64d168aa5e83b5f4f11243dc5dc2af
Andrei Zhydzenka <zhidenko1999@gmail.com>
2019-07-27 23:13:14 +0300

    Merge step13. Implemetation of add, update and delete methods in HiveSectionService.

--- KatlaSport.DataAccess/Migrations/201907212249531_AddCategoryDescription.cs
+++ KatlaSport.DataAccess/Migrations/201907212249531_AddCategoryDescription.cs
@@ -2,6 +2,9 @@ namespace KatlaSport.DataAccess.Migrations
 {
     using System.Data.Entity.Migrations;
 
+    /// <summary>
+    /// partial class
+    /// </summary>
     public partial class AddCategoryDescription : DbMigration
     {
         public override void Up()
